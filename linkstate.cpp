3548e5c1e4292f72079b29a60b92b7631559bfb3
Sophia <theyuyao@gmail.com>
2014-11-08 18:09:21 -0600

    Got output in wrong order

--- linkstate.cpp
+++ linkstate.cpp
@@ -20,9 +20,9 @@ void Linkstate::make_graph(string topofile)
 			idx++;
 		}
 			
-		pair<int, int> edge_1(ints[1], ints[2]);
+		pair<int, int> edge_1(ints[2], ints[1]);
 		graph[ints[0]].push_back(edge_1);	
-		pair<int, int> edge_2(ints[0], ints[2]);
+		pair<int, int> edge_2(ints[2], ints[0]);
 		graph[ints[1]].push_back(edge_2);
 	}
 }
@@ -35,69 +35,51 @@ void Linkstate::find_state()
 }
 
 void Linkstate::find_shortest_path(int source)
-{	
-	/*For sytantic simplicity we get references to 
-	  the dictionary and the vector that we need*/
-	unordered_map<int, pair<int, int> >* map = &distances[source];	
-	vector<pair<int, int> >* neighbors = &graph[source];
-	
-	//put source in the map
-	pair<int, int> src(source, 0);
-	(*map)[source] = src;
-
-	priority_queue<tuple<int, int, int> > pq;
-	int predecessor = source;	
-
-	//initialize priority queue
-	cout<< "size of neighbors is "<< (*neighbors).size()<<endl;
-	for(int i = 0; i < (*neighbors).size(); i++)
-	{
-		//grab the next neighbor
-		pair<int, int> next = (*neighbors)[i];
-		//make a new pair of (weight*node) to and add it to the prioity queue
-		cout<< "curr_node: "<< next.first<< " weight: "<< next.second<< " pred: "<< predecessor<< endl;
-
-		tuple<int, int, int> np(next.second,  next.first, predecessor);		
-		pq.push(np);
-	}	
-	
-	//main loop
-	while(!pq.empty())
-	{
-		//get next best (weight*node*predecessor)
-		tuple<int, int, int> best = pq.top();		
-		pq.pop();
-
-		int curr_node = get<1>(best);
-		int curr_weight = get<0>(best);
-		int curr_pred = get<2>(best);
-		
-		/*check to see if current node being considerd has already logged*/
-		if((*map).find(curr_node)==(*map).end())
-		{
-			pair<int, int>	temp(curr_pred, curr_weight);
-			(*map)[curr_node] = temp;
-		}	
-
-		//grab the vector for neighbors of best
-		vector<pair<int, int> >* best_neighbors = &graph[curr_node];
-		predecessor = curr_node;
-
-		//add best's neighbors to the priority queue
-		for(int i = 0; i < (*best_neighbors).size(); i++)
-		{
-			//grab the next neighbor
-			pair<int, int> next = (*best_neighbors)[i];
-			cout<< "curr_node: "<< next.first<< " weight: "<< next.second<< " pred: "<< predecessor<< endl;
-			//make a new pair of (weight*node) to and add it to the prioity queue
-			
-			if((*map).find(curr_node)==(*map).end())
-			{
-				tuple<int, int, int> np(next.second,  next.first, predecessor);		
-				pq.push(np);
-			}
-		}	
+{
+	unordered_map<int, bool> visited; //TRUE if visited, FALSE if not
+	unordered_map<int, pair<int, int> > * dist=new unordered_map<int, pair<int, int> >;//copy over to &distances[source];
+	pair<int, int> srcnode(0, source); //predecessor distance
+	(*dist)[source]=srcnode;
+	priority_queue<pair<int, int> > pq;
+	pq.push(srcnode);
+	for(auto val : graph){
+		if(val.first!=source){
+			pair<int, int> n(INT_MIN+1, val.first);
+			pair<int, int>nn(INT_MAX, val.first);
+			(*dist)[val.first]=nn;
+			pq.push(n); //add all the nodes
+		}
 	}
+	int predecessor=source;
+	while(!pq.empty()){
+		pair<int, int> curnode = pq.top();
+		curnode.first=(-1)*curnode.first;
+		pq.pop();
+		int curnodeint=curnode.second;
+		visited[curnodeint]=true;
+		vector<pair<int, int> > * neighbors=&graph[curnodeint];
+		for(int i=0;i<neighbors->size();i++){
+			int neighborint=(*neighbors)[i].second;
+			//default value of bool is false, so don't have to worry about new entry'
+			if(!visited[neighborint]){
+				int potentialnewdist=INT_MAX;
+				if(curnode.first!=INT_MAX){
+					potentialnewdist=curnode.first + (*neighbors)[i].first;
+				}
+				if (potentialnewdist < (*dist)[neighborint].first){
+					(*dist)[neighborint].first=potentialnewdist;
+					(*dist)[neighborint].second=curnodeint;
+					pair<int, int> neighbor_despair((-1)*(*dist)[neighborint].first, neighborint);
+					pq.push(neighbor_despair);
+				}
+			}
+		}
+	}
+	if(distances[source]!=NULL){
+		delete distances[source];
+		distances[source]=NULL;
+	}
+	distances[source]=dist;
 }
 
 void Linkstate::update_graph()
@@ -160,7 +142,7 @@ void Linkstate::print_graph()
 
 void Linkstate::print_routing_table(int source)
 {
-	unordered_map< int, pair<int, int> >* table = &distances[source];
+	unordered_map< int, pair<int, int> >* table = distances[source];
 	for(unordered_map<int,  pair<int, int> >::iterator it = (*table).begin(); it != (*table).end(); it++)
 	{
 		pair<int, int> data = it->second;
@@ -188,8 +170,8 @@ int main(int argc, char *argv[])
 {
 	Linkstate ls;
 	ls.make_graph(argv[1]);	
-	ls.print_graph();
+	//ls.print_graph();
 	ls.parse_changes(argv[2]);
 	ls.find_shortest_path(2);
-	ls.print_routing_table(2);
+	//ls.print_routing_table(2);
 }
--- linkstate.h
+++ linkstate.h
@@ -11,6 +11,7 @@
 #include <sstream>
 #include <unordered_map>
 #include <tuple>
+#include <limits.h>
 
 using namespace std;
 
@@ -29,14 +30,41 @@ class Linkstate
 		void print_routing_table(int source);
 
 	private:
-		/*source->neighbor->cost*/
+		/*source->cost*neighbor*/
 		unordered_map<int, vector<pair<int, int> > > graph;
-		/*source-> dest-> (predecessor*distance) */
-		unordered_map< int, unordered_map<int, pair<int, int> > > distances;
+		/*source-> dest-> (distance, predecessor) */
+		unordered_map< int, unordered_map<int, pair<int, int> >* > distances;
 		/*source->neighbor->newcost*/
-		vector<tuple<int, int, int> > changes; //hello
+		vector<tuple<int, int, int> > changes;
 
 };
 
 
 #endif /*linkstate.h*/
+/*
+1  function Dijkstra(Graph, source):
+2      dist[source] := 0                     // Initializations
+3      for each vertex v in Graph:           
+4          if v ≠ source
+5              dist[v] := infinity           // Unknown distance from source to v
+6              previous[v] := undefined      // Predecessor of v
+7          end if
+8          Q.add_with_priority(v,dist[v])
+9      end for 
+10
+11
+12     while Q is not empty:                 // The main loop
+13         u := Q.extract_min()              // Remove and return best vertex
+14         mark u as scanned
+15         for each neighbor v of u:
+16             if v is not yet scanned:
+17                 alt = dist[u] + length(u, v) 
+18                 if alt < dist[v]
+19                     dist[v] := alt
+20                     previous[v] := u
+21                     Q.decrease_priority(v,alt)
+22                 end if
+23             end if
+24         end for
+25     end while
+26     return previous[]*/
